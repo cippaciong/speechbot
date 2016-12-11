fbb88115197fa23c1c6c1d624b923901d8cd36bc
Tommaso Sardelli <lacapannadelloziotom@gmail.com>
2016-12-11 23:00:00 +0100

    Wrap conversion in if statement

--- speechbot.rb
+++ speechbot.rb
@@ -18,41 +18,43 @@ Telegram::Bot::Client.run(token, logger: Logger.new($stderr)) do |bot|
   bot.logger.info('Bot has been started')
   bot.listen do |message|
     # catch voice messages
-    if message.voice
-      # Download voice message
-      file_path = bot.api.get_file(file_id: message.voice.file_id)['result']['file_path']
-      voice = "/tmp/#{file_path}"
-      raw = voice.gsub(/oga/, 'wav')
-      open(voice, 'wb') do |file|
-        file << open("https://api.telegram.org/file/bot#{token}/#{file_path}").read
-      end
-    end
-
-    # Catch audio documents
-    if message.document
-      # Download audio file
-      file_path = bot.api.get_file(file_id: message.document.file_id)['result']['file_path']
-      # Make sure is an opus file
-      if file_path.include? 'opus'
+    if message.voice || message.document
+      if message.voice
+        # Download voice message
+        file_path = bot.api.get_file(file_id: message.voice.file_id)['result']['file_path']
         voice = "/tmp/#{file_path}"
-        raw = voice.gsub(/opus/, 'wav')
+        raw = voice.gsub(/oga/, 'wav')
         open(voice, 'wb') do |file|
           file << open("https://api.telegram.org/file/bot#{token}/#{file_path}").read
         end
       end
+
+      # Catch audio documents
+      if message.document
+        # Download audio file
+        file_path = bot.api.get_file(file_id: message.document.file_id)['result']['file_path']
+        # Make sure is an opus file
+        if file_path.include? 'opus'
+          voice = "/tmp/#{file_path}"
+          raw = voice.gsub(/opus/, 'wav')
+          open(voice, 'wb') do |file|
+            file << open("https://api.telegram.org/file/bot#{token}/#{file_path}").read
+          end
+        end
+      end
+
+      # Create Audio Struct
+      audio = Struct::Audio.new(voice, raw, 16_000, :raw, 1, 'it_IT')
+
+      # Convert Audio
+      Converter.convert(audio)
+
+      # Recognize audio
+      text = Recognizer.recognize(audio)
+
+      # Send reply
+      bot.api.send_message(chat_id: message.chat.id, text: text)
     end
-
-    # Create Audio Struct
-    audio = Struct::Audio.new(voice, raw, 16_000, :raw, 1, 'it_IT')
-
-    # Convert Audio
-    Converter.convert(audio)
-
-    # Recognize audio
-    text = Recognizer.recognize(audio)
-
-    # Send reply
-    bot.api.send_message(chat_id: message.chat.id, text: text)
 
     case message.text
     when /start/
