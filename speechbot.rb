4142a76efb07bd5871f43d3bd3059a543b3b7daa
Tommaso Sardelli <lacapannadelloziotom@gmail.com>
2016-12-11 21:30:24 +0100

    Transcribe also audio documents shared from whatsapp

--- speechbot.rb
+++ speechbot.rb
@@ -16,9 +16,6 @@ Telegram::Bot::Client.run(token, logger: Logger.new($stderr)) do |bot|
 
     # return the audio id
     if message.voice
-      # Define Struct
-      Struct.new("Audio", :voice, :raw, :rate, :encoding, :channels, :language)
-
       # Download voice message
       file_path = bot.api.get_file(file_id: message.voice.file_id)["result"]["file_path"]
       require 'open-uri'
@@ -36,6 +33,27 @@ Telegram::Bot::Client.run(token, logger: Logger.new($stderr)) do |bot|
       text = Recognizer.recognize(audio)
       bot.api.send_message(chat_id: message.chat.id, text: text)
     end
+
+    # return the audio id
+    if message.document
+      # Download audio file
+      file_path = bot.api.get_file(file_id: message.document.file_id)["result"]["file_path"]
+      require 'open-uri'
+      voice = "/tmp/#{file_path}"
+      raw = voice.gsub(/opus/, 'wav')
+      open(voice, 'wb') do |file|
+        file << open("https://api.telegram.org/file/bot#{token}/#{file_path}").read
+      end
+      
+      # Create Audio Struct
+      audio = Struct::Audio.new(voice, raw, 16000, :raw, 1, "it_IT")
+
+      #Convert Audio
+      Converter.convert(audio)
+      text = Recognizer.recognize(audio)
+      bot.api.send_message(chat_id: message.chat.id, text: text)
+    end
+
 
     case message.text
     when /start/
