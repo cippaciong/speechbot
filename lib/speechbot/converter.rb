07a9c8e145dbb105991c0e600b18786dd97ae3ca
Tommaso Sardelli <lacapannadelloziotom@gmail.com>
2016-12-18 15:38:45 +0100

    Check that the audio is opus before attempting to convert it

--- lib/speechbot/converter.rb
+++ lib/speechbot/converter.rb
@@ -2,10 +2,26 @@ require 'ruby-sox'
 
 module Converter
   def self.convert(audio)
-    sox = Sox::Cmd.new
-                  .add_input(audio.voice)
-                  .set_output(audio.raw)
-                  .set_effects(rate: audio.rate, channels: audio.channels)
-    sox.run
+    if self.opus?(audio)
+      sox = Sox::Cmd.new
+                    .add_input(audio.voice)
+                    .set_output(audio.raw)
+                    .set_effects(rate: audio.rate, channels: audio.channels)
+      sox.run
+    else
+      'Sorry, only Opus (ogg) files are supported'
+    end
   end
+
+  private
+  def self.opus?(audio)
+    # Suppress stderr messages
+    $stderr.reopen("/dev/null", "w")
+
+    # Run sox --i and checks if output contains 'Opus'
+    IO.popen(['sox', '--i', audio], 'r') do |f|
+      return !f.gets(nil).to_s.scan('Opus').empty?
+    end
+  end
+
 end
