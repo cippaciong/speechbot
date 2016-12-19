23292e2d8d7fc57a29d4d8b70283a01b031770b6
Tommaso Sardelli <lacapannadelloziotom@gmail.com>
2016-12-19 16:10:50 +0100

    Add test for empty and valid file for converter

--- test/converter_test.rb
+++ test/converter_test.rb
@@ -1,12 +1,24 @@
+lib = File.expand_path('../../lib/', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'speechbot/converter'
 
 class ConverterTest < Minitest::Test
 
-  def test_convert
-    assert_equal "Sorry, only Opus (ogg) files are supported",
-      Converter.convert('/tmp/not_ogg')
+  def test_truth
+    assert true
+  end
+
+  def test_empty_file
+    assert_equal 'Wrong input file encoding',
+      Converter.convert('/tmp/not_ogg').value
+  end
+
+  def test_valid_file
+    assert_equal '/tmp/audio.wav',
+      Converter.convert('/tmp/audio.ogg').value
   end
 
   #def test_predefined_answers_is_not_empty
