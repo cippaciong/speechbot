f20a026fdc4b931618bb2df2045baed9b5fc612d
Tommaso Sardelli <lacapannadelloziotom@gmail.com>
2016-12-22 11:19:24 +0100

    Create audio and document directories upon startup

--- app.rb
+++ app.rb
@@ -1,5 +1,3 @@
-#! /usr/bin/env ruby
-
 lib = File.expand_path('../lib/', __FILE__)  
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
@@ -7,11 +5,18 @@ require 'rack'
 require 'rack/server'
 require 'json'
 require 'logger'
+require 'fileutils'
 require 'speechbot'
 
 
 class App
   @bot = SpeechBot.new
+
+  # Create audio and document directories
+  FileUtils.mkdir_p('/tmp/audio')
+  FileUtils.mkdir_p('/tmp/document')
+
+  # Get requests
   def self.call(env)
     request = Rack::Request.new env
 
