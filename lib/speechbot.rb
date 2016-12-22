679b268363045171c7ec37122723a0ea0142a72a
Tommaso Sardelli <lacapannadelloziotom@gmail.com>
2016-12-22 11:32:12 +0100

    Split read_and_reply in smaller methods

--- lib/speechbot.rb
+++ lib/speechbot.rb
@@ -9,13 +9,44 @@ require 'speechbot/container'
 class SpeechBot
   # Load env vars
   Dotenv.load
-  # Define Audio Struct
-  Struct.new('Audio', :voice, :raw, :rate, :encoding, :channels, :language)
 
   def initialize
     # Set bot token
     @token = ENV['BOT_TOKEN']
     @bot = Telegram::Bot::Client.new(@token, logger: Logger.new('requests.log'))
+  end
+
+  def download_file(message)
+    file = @bot.api.get_file(file_id: message.voice.file_id)['result']['file_path']
+    input_path = "/tmp/#{file}"
+    open(input_path, 'wb') do |input|
+      input << open("https://api.telegram.org/file/bot#{@token}/#{file}").read
+    end
+    return input_path
+  end
+
+  def recognize(message)
+    input_path = download_file(message)
+    # Initialize transaction
+    convert_ogg = Dry.Transaction(container: Container) do
+      step :convert
+      step :recognize
+    end 
+    # Send reply
+    convert_ogg.call(input_path, recognize: ["it_IT"]).value
+  end
+
+  def read_and_reply(message)
+    # Catch voice messages or audio files
+    if message.voice || message.document
+      @bot.api.send_message(chat_id: message.chat.id, text: recognize(message))
+    end
+
+    case message.text
+    when /start/
+      @bot.api.send_message(chat_id: message.chat.id,
+                            text: "Hello, #{message.from.first_name}")
+    end
   end
 
   def update(data)
@@ -27,60 +58,4 @@ class SpeechBot
     end
   end
 
-  # Define helper methods
-  def prepare_reply(voice)
-    # audio = Struct::Audio.new(voice, raw, 16_000, :raw, 1, 'it_IT')
-    raw = convert(voice)
-    recognize(raw)
-  end
-
-  def convert(audio)
-    Converter.convert(audio).value
-  end
-
-  def recognize(audio)
-    Recognizer.recognize(audio, "it_IT").value
-  end
-
-  def read_and_reply(message)
-      # Catch voice messages or audio files
-      if message.voice || message.document
-        file = @bot.api.get_file(file_id: message.voice.file_id)['result']['file_path']
-        input_path = "/tmp/#{file}"
-        open(input_path, 'wb') do |input|
-          input << open("https://api.telegram.org/file/bot#{@token}/#{file}").read
-        end
-
-        convert_ogg = Dry.Transaction(container: Container) do
-          step :convert
-          step :recognize
-        end 
-        # Send reply
-        text = convert_ogg.call(input_path, recognize: ["it_IT"]).value
-        @bot.api.send_message(chat_id: message.chat.id, text: text)
-      end
-
-        ## Catch audio documents
-        #if message.document
-          ## Download audio file
-          #file_path = @bot.api.get_file(file_id: message.document.file_id)['result']['file_path']
-          ## Make sure is an opus file
-          #if file_path.include? 'opus'
-            #voice = "/tmp/#{file_path}"
-            #raw = voice.gsub(/opus/, 'wav')
-            #open(voice, 'wb') do |file|
-              #file << open("https://api.telegram.org/file/bot#{@token}/#{file_path}").read
-            #end
-          ## Send reply
-          #text = prepare_reply(voice)
-          #@bot.api.send_message(chat_id: message.chat.id, text: text)
-          #end
-        #end
-      #end
-
-      case message.text
-      when /start/
-        @bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
-      end
-  end
 end
