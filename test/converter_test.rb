37d17356eeb3bf0f3f987425014e3055663435ec
Tommaso Sardelli <lacapannadelloziotom@gmail.com>
2016-12-22 17:07:33 +0100

    Create audio samples to be used in tests

--- test/converter_test.rb
+++ test/converter_test.rb
@@ -3,21 +3,17 @@ require 'speechbot/converter'
 
 class ConverterTest < Minitest::Test
 
-  def test_truth
-    assert true
-  end
-
   def test_empty_file
     assert_equal 'Wrong input file encoding',
-      Converter.convert('/tmp/not_ogg').value
+      Converter.convert('test/samples/not_ogg').value
   end
 
   def test_valid_file
-    assert_equal '/tmp/audio.wav',
-      Converter.convert('/tmp/audio.ogg').value
+    assert_equal 'test/samples/audio.wav',
+      Converter.convert('test/samples/audio.ogg').value
   end
-
-  #def test_predefined_answers_is_not_empty
-    #refute_empty MagicBall::ANSWERS
-  #end
+  def test_long_file
+    assert_equal 'test/samples/audio_too_long.wav',
+      Converter.convert('test/samples/audio_too_long.ogg').value
+  end
 end
--- test/recognizer_test.rb
+++ test/recognizer_test.rb
@@ -3,7 +3,17 @@ require 'speechbot/recognizer'
 
 class RecognizerTest < Minitest::Test
 
-  def test_truth
-    assert true
+  def test_invalid_encoding
+    assert_equal 'Wrong input file encoding',
+      Recognizer.recognize('test/samples/not_ogg', 'it_IT').value
+  end
+
+  def test_valid_file
+    assert_equal 'Vanna storna',
+      Recognizer.recognize('test/samples/audio.wav', 'it_IT').value
+  end
+  def test_file_too_long
+    assert_equal 'Files longer than 1 minute are not supported for now, sorry :(',
+      Recognizer.recognize('test/samples/audio_too_long.wav', 'it_IT').value
   end
 end
